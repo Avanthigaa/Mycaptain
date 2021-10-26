a072dba10c634e33b6cbacf29fc642071df0b7a6
Avanthigaa <54901070+Avanthigaa@users.noreply.github.com>
2021-10-26 19:29:37 +0530

    Add files via upload

---
+++ C/Conditional Statements 2/switch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int choice;
+    printf("MENU\n\n1.Pizza\n2.Burger\n3.Pasta\n4.French Fries\n5.Sandwich\n\nEnter your choice : ");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+            printf("\nFood item - Pizza\nPrice - Rs 239");
+            break;
+        case 2:
+            printf("\nFood item - Burger\nPrice - Rs 129");
+            break;
+        case 3:
+            printf("\nFood item - Pasta\nPrice - Rs 179");
+            break;
+        case 4:
+            printf("\nFood item - French Fries\nPrice - Rs 99");
+            break;
+        case 5:
+            printf("\nFood item - Sandwich\nPrice - Rs 149");
+            break;
+        default : 
+            printf("Invalid choice");
+            break;
+    }
+    
+    
+
+    return 0;
+}
